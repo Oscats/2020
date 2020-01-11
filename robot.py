4ce387b9e772052232437eae3dd99115b26858c2
OSCATS-FIDO\Oscats <Jacobtest@hdsd.org>
2020-01-11 15:06:34 -0500

    Fixed Mecha Drive, fully operational. Controls have also changed.

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import wpilib
 from wpilib.drive import MecanumDrive
-
+from wpilib.interfaces import GenericHID
+Hand = GenericHID.Hand
 
 class MyRobot(wpilib.SampleRobot):
     # Channels on the roboRIO that the motor controllers are plugged in to
@@ -45,7 +46,7 @@ class MyRobot(wpilib.SampleRobot):
             # Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
             # This sample does not use field-oriented drive, so the gyro input is set to zero.
             self.drive.driveCartesian(
-                (self.speed*self.stick.getX()), (self.speed*self.stick.getY()), (self.speed*self.stick.getRawAxis(5))
+                (self.speed*self.stick.getX()), (self.speed*self.stick.getY()), (self.speed*self.stick.getX(Hand.kLeft))
             )
 
             wpilib.Timer.delay(0.005)  # wait 5ms to avoid hogging CPU cycles
