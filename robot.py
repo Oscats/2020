4c83f91747da089c09ae830e181e3fe245b4bd97
Kaja33 <59773972+Kaja33@users.noreply.github.com>
2020-01-11 13:42:04 -0500

    Create robot.py
    First attempt at Mech Drive

---
+++ robot.py
@@ -0,0 +1,55 @@
+import wpilib
+from wpilib.drive import MecanumDrive
+
+
+class MyRobot(wpilib.SampleRobot):
+    # Channels on the roboRIO that the motor controllers are plugged in to
+    frontLeftChannel = 0
+    rearLeftChannel = 2
+    frontRightChannel = 1
+    rearRightChannel = 3
+
+    # The channel on the driver station that the joystick is connected to
+    joystickChannel = 0
+
+    def robotInit(self):
+        """Robot initialization function"""
+        self.frontLeftMotor = wpilib.Talon(self.frontLeftChannel)
+        self.rearLeftMotor = wpilib.Talon(self.rearLeftChannel)
+        self.frontRightMotor = wpilib.Talon(self.frontRightChannel)
+        self.rearRightMotor = wpilib.Talon(self.rearRightChannel)
+
+        # invert the left side motors
+        self.frontLeftMotor.setInverted(True)
+
+        # you may need to change or remove this to match your robot
+        self.rearLeftMotor.setInverted(True)
+
+        self.drive = MecanumDrive(
+            self.frontLeftMotor,
+            self.rearLeftMotor,
+            self.frontRightMotor,
+            self.rearRightMotor,
+        )
+
+        self.drive.setExpiration(0.1)
+
+        self.stick = wpilib.XboxController(self.joystickChannel)
+        self.speed = 0.5
+
+    def operatorControl(self):
+        """Runs the motors with Mecanum drive."""
+
+        self.drive.setSafetyEnabled(True)
+        while self.isOperatorControl() and self.isEnabled():
+            # Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+            # This sample does not use field-oriented drive, so the gyro input is set to zero.
+            self.drive.driveCartesian(
+                (self.speed*self.stick.getX()), (self.speed*self.stick.getY()), (self.speed*self.stick.getRawAxis(5))
+            )
+
+            wpilib.Timer.delay(0.005)  # wait 5ms to avoid hogging CPU cycles
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
